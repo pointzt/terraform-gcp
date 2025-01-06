43ccc68f34d7edcf2068179d7bbbe0dbe459eeb1
pointzt <garyaxel44@gmail.com>
2025-01-06 16:01:09 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "google_compute_instance" "default" {
 
   boot_disk {
     initialize_params {
-      image = "ubuntu-minimal-2210-kinetic-amd64-v20230126"
+      image = "debian-cloud/debian-11"
     }
   }
 
