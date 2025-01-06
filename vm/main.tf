7502cd1a81cb0235e462f564cd04b7fbd1cde1b4
pointzt <garyaxel44@gmail.com>
2025-01-06 13:33:00 +0700

    Update main.tf

--- vm/main.tf
+++ vm/main.tf
@@ -1,14 +1,18 @@
-resource "google_compute_instance" "terraform" {
-  name         = "terraform"
+# Create a Google Compute instance
+
+resource "google_compute_instance" "default" {
+  name         = "gary-vm"
   machine_type = "e2-micro"
+  zone         = "us-central1-a"
+
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
     }
   }
+
   network_interface {
     network = "default"
-    access_config {
-    }
+    access_config {}
   }
 }
